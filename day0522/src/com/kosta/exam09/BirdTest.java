7a9422006d8805e52d8675b66ab239ddb85ac459
donggyun <dky7034@naver.com>
2024-05-23 08:41:36 +0900

    240523 commit

--- day0522/src/com/kosta/exam09/BirdTest.java
+++ day0522/src/com/kosta/exam09/BirdTest.java
@@ -14,9 +14,9 @@ class Dove extends Bird {
 public class BirdTest {
     public static void main(String[] args) {
 //        Bird bird = new Bird();
-        // 추상클래스는 바디가 구체화되지 않는 메서드가 있으므로 객체 생성을 할 수 없습니다...
+        // 추상클래스는 바디가 구체화되지 않는 메서드가 있으므로 객체 생성을 할 수 없습니다.
         Bird b = new Dove();
         // 상속관계에서 자식클래스는 부모클래스에 포함관계에 있으므로
-        // 부모클래스의 참조변수는 자식클래스의 객체를 참조할 수 있습니다...
+        // 부모클래스의 참조변수는 자식클래스의 객체를 참조할 수 있습니다.
     }
 }
