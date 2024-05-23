cb124099d875a162073888a7b4a2484eb3e8ce29
artp <144507332+dky7034@users.noreply.github.com>
2024-05-23 14:36:17 +0900

    commit

--- day0523/src/com/kosta/exam04/StudentTest.java
+++ day0523/src/com/kosta/exam04/StudentTest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StudentTest {
+    private static Scanner sc = new Scanner(System.in);
     public static void menu() {
         System.out.println("1. 추가, 2. 검색, 3. 삭제, 0. 종료");
         System.out.println("메뉴를 선택하세요 ==> ");
@@ -11,7 +12,6 @@ public class StudentTest {
     // 메서드
     private static void insertStudent (ArrayList<Student> students) {
         String name, address, phone;
-        Scanner sc = new Scanner(System.in);
         System.out.println("*** 학생 정보 추가 ***");
         System.out.print("이름을 입력하세요: ");
         name = sc.next();
@@ -23,20 +23,44 @@ public class StudentTest {
         Student s = new Student(name, address, phone);
         students.add(s);
         System.out.println("학생의 정보를 추가하였습니다.");
-
         System.out.println("----------------------");
 
     };
-    private static void searchStudent (ArrayList<Student> students) {};
+    // 메서드
+    private static void searchStudent (ArrayList<Student> students) {
+        String name, phone;
+        System.out.println("*** 학생 정보 검색 ***");
+//        System.out.print("검색할 학생의 이름을 입력하세요 ==> ");
+//        System.out.println();
+        System.out.print("검색할 학생의 전화번호를 입력하세요 ==> ");
+        phone = sc.next();
+        for (Student s : students) {
+            if (s.getPhone().equals(phone)) {
+                System.out.println(s);
+            }
+        }
+    };
 
-    private static void deleteStudent (ArrayList<Student> students) {};
+    // 메서드
+    private static void deleteStudent (ArrayList<Student> students) {
+        String name, phone;
+        System.out.println("*** 학생 정보 검색 ***");
+//        System.out.print("삭제할 학생의 이름을 입력하세요 ==> ");
+        System.out.print("삭제할 학생의 전화번호를 입력하세요 ==> ");
+        phone = sc.next();
+        for (Student s : students) {
+            if (s.getPhone().equals(phone)) {
+                System.out.println(s);
+                students.remove(s);
+            }
+        }
+    };
 
 
     public static void main(String[] args) {
         ArrayList<Student> students = new ArrayList<Student>();
         int sel;
         String name, address, phone;
-        Scanner sc = new Scanner(System.in);
 
         while (true) {
             menu();
